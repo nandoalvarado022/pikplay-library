b991bd2bd6c717d5f73c54e976790e9ef8096080
Hernando Luis Alvarado Almario <hernando.halvarado@mercadolibre.com.co>
2024-10-20 16:40:14 -0500

    init

---
+++ next.config.js
@@ -0,0 +1,32 @@
+/** @type {import('next').NextConfig} */
+
+///* eslint-disable import/no-anonymous-default-export */
+// import NextFederationPlugin from '@module-federation/nextjs-mf';
+
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
+
+module.exports = {
+    webpack(config, options) {
+        const { isServer } = options;
+        if (isServer) {
+            config.optimization.usedExports = false
+        }
+        config.plugins.push(
+            new NextFederationPlugin({
+                name: 'pikplay_library',
+                remotes: {
+                    next1: `pikplay_frontend@http://localhost:3001/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+                },
+                filename: 'static/chunks/remoteEntry.js',
+                exposes: {
+                    './Header': './src/components/Header.jsx',
+                },
+                shared: {
+                    // whatever else
+                },
+            }),
+        );
+
+        return config;
+    },
+};
---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+
+const Index = () => {
+    return (
+        <div>
+        <h1>Home</h1>
+        <Header />
+        </div>
+    );
+}
+
+export default Index;
